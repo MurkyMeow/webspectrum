7f6827e2d58f0373167def62dce4057208bba205
MurkyMeow <dinosowermurky@gmail.com>
2021-07-10 02:00:38 +0300

    add my thoughts on dft, stick to a simple algorithm for now

--- core/dft.c
+++ core/dft.c
@@ -5,15 +5,12 @@
 void dft(float* output, const short* input, int sample_count) {
   for (int k = 0; k < sample_count; k += 1) {
     float freq = M_PI * 2 * (float)k / (float)sample_count;
-    float real = 0;
-    float imag = 0;
+    float correlation = 0;
 
     for (int t = 0; t < sample_count; t += 1) {
-      float arg = freq * (float)t;
-      real += (float)input[t] * cosf(arg);
-      imag += -(float)input[t] * sinf(arg);
+      correlation += (float)input[t] * cosf(freq * (float)t);
     }
 
-    output[k] = sqrtf(real * real + imag * imag) / (float)sample_count;
+    output[k] = fabsf(correlation) / (float)sample_count;
   }
 }
